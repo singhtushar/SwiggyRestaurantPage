938d2248e8e3a492adf7aa4b838affe1cab68c89
Tushar Singh <singhtusharts22@gmail.com>
2022-02-24 12:56:30 +0530

    item quantity monitoring functionality added

--- script.js
+++ script.js
@@ -11,3 +11,38 @@ for (let i = 0; i < dropdown.length; i++) {
         }
     });
 }
+
+const addItem = document.getElementsByClassName('increment-item');
+const decrementItem = document.getElementsByClassName('decrement-item');
+
+function wrapper1() {
+
+    // console.log(addItem);
+    // console.log(decrementItem);
+    
+    for(let i=0;i<addItem.length;i++) {
+        let count=0;
+        addItem[i].addEventListener('click', function() {
+            const div = this.previousElementSibling;
+            count++;
+    
+            div.innerText = `${count}`;
+            
+        })
+
+        decrementItem[i].addEventListener('click', function() {
+            const div = this.nextElementSibling;
+            count--;
+            count = Math.max(count, 0);
+            if(count <= 0)
+                div.innerText='ADD';
+            else
+            div.innerText = `${count}`;
+        })
+    }
+
+}
+
+wrapper1();
+
+
