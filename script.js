a3852d35d6846b3c3553c43f4f9a83d276b39a84
Tushar Singh <singhtusharts22@gmail.com>
2022-03-01 12:08:21 +0530

    cart display added

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ function wrapper1() {
 
     // console.log(addItem);
     // console.log(decrementItem);
+
+    let itemsInCart = {}; // it will go into model
+    console.log(addItem);
     
     for(let i=0;i<addItem.length;i++) {
         let count=0;
@@ -26,7 +29,18 @@ function wrapper1() {
             const div = this.previousElementSibling;
             count++;
     
-            div.innerText = `${count}`;
+            div.innerHTML = `&nbsp;&nbsp;${count}&nbsp;&nbsp;`;
+
+            // console.log(div.getAttribute('name'));
+
+            const itemName = div.getAttribute('name');
+            if(itemsInCart[`${itemName}`] === undefined)
+            itemsInCart[`${itemName}`] = 1;
+            else
+            itemsInCart[`${itemName}`]++;
+            // console.log(itemsInCart);
+
+            displayCartItems(itemsInCart);
             
         })
 
@@ -37,12 +51,66 @@ function wrapper1() {
             if(count <= 0)
                 div.innerText='ADD';
             else
-            div.innerText = `${count}`;
+            div.innerHTML = `&nbsp;&nbsp;${count}&nbsp;&nbsp;`;
+
+            // console.log(div.getAttribute('name'));
+
+            const itemName = div.getAttribute('name');
+            if(itemsInCart[`${itemName}`] === undefined || itemsInCart[`${itemName}`]<=0)
+                delete itemsInCart[`${itemName}`];
+            else{
+                itemsInCart[`${itemName}`]--;
+                if(itemsInCart[`${itemName}`]<=0)
+                delete itemsInCart[`${itemName}`];
+            }
+            // console.log(itemsInCart)
+
+            displayCartItems(itemsInCart);
+
         })
+
     }
 
 }
 
 wrapper1();
+
+function displayCartItems(itemsInCart) {
+    const x = document.querySelector('#items-present');
+    x.style.display = 'none';
+    const mainDiv = document.querySelector('.display-items');
+    mainDiv.innerHTML = "";
+    for(let item in itemsInCart) {
+        // console.log(`${item}: ${itemsInCart[item]}`)
+        const divp = document.createElement('div');
+        divp.setAttribute('class', 'item-quantity');
+        divp.style.border = '1px solid black';
+        const p1 = document.createElement('div');
+        const p2 = document.createElement('div');
+        const p3 = document.createElement('div');
+        p1.innerHTML = '-';
+        p1.setAttribute('class', 'decrement-item');
+        // console.log(p1);
+
+        p2.innerHTML = `${itemsInCart[item]}`
+        p2.setAttribute("name", `${item}`);
+        p2.setAttribute("class", "ordered-item");
+        p2.setAttribute("class", "item-quantity");
+        // p.innerHTML = `${item}: ${itemsInCart[item]}`;
+        p3.innerHTML = '+';
+        p3.setAttribute("class", "increment-item");
+        const span = document.createElement('span');
+        span.innerHTML = `${item}&nbsp;&nbsp;`;
+        // console.log(p3);
+        divp.appendChild(span);
+        divp.appendChild(p1);
+        divp.appendChild(p2);
+        divp.appendChild(p3);
+        mainDiv.appendChild(divp);
+    }
+
+}
+
+
 
 
