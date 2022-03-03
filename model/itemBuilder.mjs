8358205a9626341fb49d4b14f591933432a98acf
Tushar Singh <singhtusharts22@gmail.com>
2022-03-03 18:31:00 +0530

    controllers and models added

---
+++ model/itemBuilder.mjs
@@ -0,0 +1,65 @@
+function Item (name, isVeg, isBestSeller, price, description, image, category) {
+    this.name = name;
+    this.isVeg = isVeg;
+    this.isBestSeller = isBestSeller;
+    this.price = price;
+    this.description = description;
+    this.image = image;
+    this.category = category;
+}
+
+// Builder class for each item
+
+class ItemBuilder {
+
+    constructor() {
+        this.name = '';
+        this.isVeg = false;
+        this.isBestSeller = false;
+        this.price = 0;
+        this.description = "";
+        this.image = "";
+        this.category = "";
+    }
+
+    setName(name) {
+        this.name = name;
+        return this;
+    }
+
+    setIsVeg(isVeg) {
+        this.isVeg = isVeg;
+        return this;
+    }
+
+    setIsBestSeller(isBestSeller) {
+        this.isBestSeller = isBestSeller;
+        return this;
+    }
+
+    setPrice(price) {
+        this.price = price;
+        return this;
+    }
+
+    setDescription(description) {
+        this.description = description;
+        return this;
+    }
+
+    setImage(image) {
+        this.image = image;
+        return this;
+    }
+
+    setCategory(category) {
+        this.category = category;
+        return this;
+    }
+
+    build() {
+        return new Item(this.name, this.isVeg, this.isBestSeller, this.price, this.description, this.image, this.category);
+    }
+}
+
+export { ItemBuilder };
